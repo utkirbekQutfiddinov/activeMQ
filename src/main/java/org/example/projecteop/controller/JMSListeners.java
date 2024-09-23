d49ae97ba5214fe887388af28b8eacba3419a0b9
utkirbek <utkirbekqutfiddinov@gmail.com>
2024-09-23 21:55:17 +0500

    ..

---
+++ src/main/java/org/example/projecteop/controller/JMSListeners.java
@@ -0,0 +1,24 @@
+package org.example.projecteop.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class JMSListeners {
+
+    @Async
+    @JmsListener(destination = "admin")
+    public void askjhbdashb(String message) {
+        System.out.println("message: " + message + " " + Thread.currentThread().getName());
+    }
+
+    @Async
+    @JmsListener(destination = "admin")
+    public void asasasa(String message) {
+        System.out.println("Received a new message: " + message + " " + Thread.currentThread().getName());
+    }
+
+}
---
+++ src/main/java/org/example/projecteop/controller/TextController.java
@@ -0,0 +1,30 @@
+package org.example.projecteop.controller;
+
+import jakarta.jms.JMSException;
+import jakarta.jms.TextMessage;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/jms")
+public class TextController {
+
+    private final JmsTemplate jmsTemplate;
+
+    public TextController(JmsTemplate jmsTemplate) {
+        this.jmsTemplate = jmsTemplate;
+    }
+
+    @PostMapping
+    public void yoz(@RequestParam("que") String q, @RequestParam("message") String text) {
+        jmsTemplate.convertAndSend(q, text);
+    }
+
+
+    @GetMapping
+    public String get(@RequestParam("name") String name) throws JMSException {
+        TextMessage receive = (TextMessage) jmsTemplate.receive(name);
+
+        return receive.getText();
+    }
+}
